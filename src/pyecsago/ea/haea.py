6ba1e9c224db422c766641fec813c244d1ef50c8
Joan Sebastian Tamayo Rivera <pwnaoj@gmail.com>
2024-10-01 22:44:38 -0500

    feat: Se ajusta la clase HAEA para que permita seleccionar el operador genético que se aplicará al individuo de manera probabilistica teniendo en cuenta las tasas de los operadores codificados en el individuo perse.

--- src/pyecsago/ea/haea.py
+++ src/pyecsago/ea/haea.py
@@ -6,75 +6,98 @@ from .individual import GeneraIndividuo
 
 class HAEA(OperadoresEvolutivos):
     def __init__(self, tasa_aprendizaje=None):
-        if tasa_aprendizaje is None:
-            self.tasa_aprendizaje = np.random.uniform(0, 1)
-        else:
-            self.tasa_aprendizaje = tasa_aprendizaje
+        self.tasa_aprendizaje = np.random.uniform(0, 1) if tasa_aprendizaje is None else tasa_aprendizaje
 
-    def mutar(self, individuo, tipo_mutacion='gaussiana_adaptativa'):
-        """ Aplica la mutación al individuo según el tipo especificado. """
-        if tipo_mutacion == 'gaussiana':
+    def seleccionar_operador(self, tasas_operadores):
+        """ Selecciona un operador basado en las tasas del individuo """
+        operadores = list(tasas_operadores.keys())
+        probabilidades = list(tasas_operadores.values())
+        operador_seleccionado = np.random.choice(operadores, p=probabilidades)
+
+        return operador_seleccionado
+
+    def aplicar_operador(self, individuo, padre2=None):
+        """ Aplica el operador seleccionado al individuo """
+        operador = self.seleccionar_operador(individuo.tasas_operadores)
+
+        if operador == 'mutacion_gaussiana':
             self._mutacion_gaussiana(individuo)
-        elif tipo_mutacion == 'gaussiana_adaptativa':
+        elif operador == 'mutacion_gaussiana_adaptativa':
             self._mutacion_gaussiana_adaptativa(individuo)
+        elif operador == 'cruce_lc' and padre2 is not None:
+            return self._linear_crossover(individuo, padre2)
+        elif operador == 'cruce_lcd' and padre2 is not None:
+            return self._linear_crossover_per_dimension(individuo, padre2)
         return individuo
-
-    def cruzar(self, padre1, padre2, tipo_cruce='LCD'):
-        """ Aplica el cruce entre dos padres usando el tipo especificado. """
-        if tipo_cruce == 'LC':
-            return self._linear_crossover(padre1, padre2)
-        elif tipo_cruce == 'LCD':
-            return self._linear_crossover_per_dimension(padre1, padre2)
-
-    def ajustar_tasas(self, individuo, recompensa=True):
+    
+    def ajustar_tasas(self, individuo, operador, recompensa=True):
+        """ Ajusta las tasas del operador seleccionado, recompensando o penalizando """
         if recompensa:
-            individuo.tasas_operadores *= (1.0 + self.tasa_aprendizaje)
+            individuo.tasas_operadores[operador] *= (1.0 + self.tasa_aprendizaje)
         else:
-            individuo.tasas_operadores *= (1.0 - self.tasa_aprendizaje)
-        
+            individuo.tasas_operadores[operador] *= (1.0 - self.tasa_aprendizaje)
+
+        # Normalizar tasas después de ajustar
         individuo.normalizar_tasas()
-         
+
     def evaluar_operador(self, padre, hijo):
         return hijo.fitness > padre.fitness
-
-    # Implementaciones de mutación gaussiana
+    
+    # Implementación de mutación gaussiana 
     def _mutacion_gaussiana(self, individuo):
+        """Aplica la mutación gaussiana clásica al individuo"""
+        tasa_mutacion = individuo.tasas_operadores['mutacion_gaussiana']
         for i in range(len(individuo.genoma)):
-            if np.random.rand() < individuo.tasa_mutacion:
+            if np.random.rand() < tasa_mutacion:
                 individuo.genoma[i] += np.random.normal(0, individuo.sigma2)
 
-    # Implementaciones de mutación gaussiana adaptativa
+    # Implementación de mutación gaussiana adaptativa
     def _mutacion_gaussiana_adaptativa(self, individuo):
+        """Aplica la mutación gaussiana adaptativa al individuo"""
+        tasa_mutacion_adaptativa = individuo.tasas_operadores['mutacion_gaussiana_adaptativa']
         for i in range(len(individuo.genoma)):
-            if np.random.rand() < individuo.tasa_mutacion:
+            if np.random.rand() < tasa_mutacion_adaptativa:
                 adaptacion_sigma = individuo.sigma2 * (1 + np.random.randn() * 0.1)
                 individuo.genoma[i] += np.random.normal(0, adaptacion_sigma)
 
-    # Implementaciones de cruce LC
+    # Implementación de cruce LC (Linear Crossover)
     def _linear_crossover(self, padre1, padre2):
-        """Linear Crossover entre dos padres"""
-        alpha = np.random.rand()
-        hijo1_genoma = alpha * padre1.genoma + (1 - alpha) * padre2.genoma
-        hijo2_genoma = (1 - alpha) * padre1.genoma + alpha * padre2.genoma
+        """
+        Linear Crossover entre dos padres.
+        Genera dos hijos combinando los genomas de los padres con un factor aleatorio alpha.
+        """
+        tasa_cruce_lc = padre1.tasas_operadores['cruce_lc']
+        if np.random.rand() < tasa_cruce_lc:
+            alpha = np.random.rand()  # Factor de mezcla aleatorio
+            hijo1_genoma = alpha * padre1.genoma + (1 - alpha) * padre2.genoma
+            hijo2_genoma = (1 - alpha) * padre1.genoma + alpha * padre2.genoma
 
-        # Crear dos nuevos individuos (hijos)
-        hijo1 = GeneraIndividuo(genoma=hijo1_genoma, tasa_mutacion=padre1.tasa_mutacion, tasa_cruce=padre1.tasa_cruce)
-        hijo2 = GeneraIndividuo(genoma=hijo2_genoma, tasa_mutacion=padre2.tasa_mutacion, tasa_cruce=padre2.tasa_cruce)
+            # Crear dos nuevos individuos (hijos) con el genoma cruzado y las tasas heredadas
+            hijo1 = GeneraIndividuo(genoma=hijo1_genoma, tasas_operadores=padre1.tasas_operadores)
+            hijo2 = GeneraIndividuo(genoma=hijo2_genoma, tasas_operadores=padre2.tasas_operadores)
 
-        return hijo1, hijo2  # Devolver dos hijos
-
-    # Implementaciones de cruce LCD
+            return hijo1, hijo2  # Devolver los dos hijos
+        return padre1, padre2
+    
+    # Implementación de cruce LCD (Linear Crossover per Dimension)
     def _linear_crossover_per_dimension(self, padre1, padre2):
-        """Linear Crossover per Dimension"""
-        nuevo_genoma1 = np.copy(padre1.genoma)
-        nuevo_genoma2 = np.copy(padre2.genoma)
-        for i in range(len(padre1.genoma)):
-            alpha = np.random.rand()
-            nuevo_genoma1[i] = alpha * padre1.genoma[i] + (1 - alpha) * padre2.genoma[i]
-            nuevo_genoma2[i] = (1 - alpha) * padre1.genoma[i] + alpha * padre2.genoma[i]
+        """
+        Linear Crossover per Dimension.
+        Realiza un cruce independiente por cada dimensión del genoma, utilizando un alpha distinto para cada uno.
+        """
+        tasa_cruce_lcd = padre1.tasas_operadores['cruce_lcd']
+        if np.random.rand() < tasa_cruce_lcd:
+            nuevo_genoma1 = np.copy(padre1.genoma)
+            nuevo_genoma2 = np.copy(padre2.genoma)
+            
+            for i in range(len(padre1.genoma)):
+                alpha = np.random.rand()  # Un alpha diferente para cada dimensión
+                nuevo_genoma1[i] = alpha * padre1.genoma[i] + (1 - alpha) * padre2.genoma[i]
+                nuevo_genoma2[i] = (1 - alpha) * padre1.genoma[i] + alpha * padre2.genoma[i]
 
-        # Crear dos nuevos individuos (hijos)
-        hijo1 = GeneraIndividuo(genoma=nuevo_genoma1, tasa_mutacion=padre1.tasa_mutacion, tasa_cruce=padre1.tasa_cruce)
-        hijo2 = GeneraIndividuo(genoma=nuevo_genoma2, tasa_mutacion=padre2.tasa_mutacion, tasa_cruce=padre2.tasa_cruce)
+            # Crear dos nuevos individuos (hijos) con el genoma cruzado y las tasas heredadas
+            hijo1 = GeneraIndividuo(genoma=nuevo_genoma1, tasas_operadores=padre1.tasas_operadores)
+            hijo2 = GeneraIndividuo(genoma=nuevo_genoma2, tasas_operadores=padre2.tasas_operadores)
 
-        return hijo1, hijo2  # Devolver dos hijos
+            return hijo1, hijo2  # Devolver los dos hijos
+        return padre1, padre2
